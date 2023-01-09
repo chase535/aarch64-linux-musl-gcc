16fc3eb3965c05e08b3924a2f9186f7be1283f75
chase535 <1932263796@qq.com>
2023-01-10 00:59:00 +0800

    Delete old files
    Signed-off-by: chase535 <1932263796@qq.com>

--- aarch64-linux-gnu/lib/gcc/aarch64-linux-gnu/13.0.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/source/gcc/configure --target=aarch64-linux-gnu --prefix=/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/output/aarch64-linux-gnu --enable-languages=c,c++ --disable-nls --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
