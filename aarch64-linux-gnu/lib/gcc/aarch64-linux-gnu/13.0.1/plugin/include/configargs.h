4e86bce9e35fff10cd1446a39a1168bfe2e94a00
chase535 <1932263796@qq.com>
2023-01-18 16:26:43 +0800

    2023.01.18 15:47:52 UTC+0800
    aarch64-linux-gnu-gcc (GCC) 13.0.1 20230118 (experimental)
    Copyright (C) 2023 Free Software Foundation, Inc.
    This is free software; see the source for copying conditions.  There is NO
    warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
    
    Signed-off-by: chase535 <1932263796@qq.com>

---
+++ aarch64-linux-gnu/lib/gcc/aarch64-linux-gnu/13.0.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/source/gcc/configure --target=aarch64-linux-gnu --prefix=/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/output/aarch64-linux-gnu --enable-languages=c,c++ --disable-nls --disable-multilib --disable-werror --with-gmp=/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/output/gmp --with-mpc=/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/output/mpc --with-mpfr=/home/runner/work/aarch64-linux-gnu-with-musl/aarch64-linux-gnu-with-musl/output/mpfr CFLAGS='-g0 -O2 -fstack-protector-strong -fno-omit-frame-pointer -D_GNU_SOURCE' CXXFLAGS='-g0 -O2 -fstack-protector-strong -fno-omit-frame-pointer -D_GNU_SOURCE' LDFLAGS='-g0 -O2 -Wl,-O2,--hash-style=both -fPIE -fPIC'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
