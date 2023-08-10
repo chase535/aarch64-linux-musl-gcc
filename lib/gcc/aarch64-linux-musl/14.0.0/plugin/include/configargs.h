2b6e25fa958ada46ad168659e4227f01a96e4cf4
chase535 <1932263796@qq.com>
2023-08-11 00:48:50 +0800

    2023.08.11 00:18:06 UTC+0800
    aarch64-linux-musl-gcc (GCC) 14.0.0 20230810 (experimental)
    Copyright (C) 2023 Free Software Foundation, Inc.
    This is free software; see the source for copying conditions.  There is NO
    warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
    
    Signed-off-by: chase535 <1932263796@qq.com>

---
+++ lib/gcc/aarch64-linux-musl/14.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/source/gcc/configure --target=aarch64-linux-musl --prefix=/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/output/toolchain --with-sysroot=/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/output/sysroot --enable-languages=c,c++ --disable-multilib --disable-bootstrap --disable-libsanitizer --disable-werror --enable-initfini-array --with-arch=armv8-a --with-abi=lp64 --enable-fix-cortex-a53-835769 --enable-fix-cortex-a53-843419 --with-gmp=/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/output/gmp --with-mpc=/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/output/mpc --with-mpfr=/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/output/mpfr --with-isl=/home/runner/work/aarch64-linux-musl-gcc/aarch64-linux-musl-gcc/output/isl";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "arch", "armv8-a" } };
