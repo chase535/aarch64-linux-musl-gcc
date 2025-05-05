b2da9ab4567fe68d5e372133a60c062c857504a7
chase535 <1932263796@qq.com>
2025-05-05 03:05:07 +0000

    aarch64-linux-musl-gcc (GCC) 16.0.0 20250505 (experimental) Copyright (C) 2025 Free Software Foundation, Inc. This is free software; see the source for copying conditions.  There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
    Signed-off-by: chase535 <1932263796@qq.com>

---
+++ aarch64-linux-musl/include/linux/version.h
@@ -0,0 +1,5 @@
+#define LINUX_VERSION_CODE 397056
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
+#define LINUX_VERSION_MAJOR 6
+#define LINUX_VERSION_PATCHLEVEL 15
+#define LINUX_VERSION_SUBLEVEL 0
